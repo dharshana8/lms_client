43b15ec4d7f58eec6030b325635576a8adf99b37
unknown <dharshanasivakumar8@gmail.com>
2025-12-04 15:51:47 +0530

    first commit

---
+++ clone/index.js
@@ -0,0 +1,23 @@
+ let a=0;
+ function cli(){
+   const heading=document.getElementsByClassName("hi")  
+     heading[0].innerText="dharshh";
+      heading[1].innerText=++a;
+
+ }
+
+ const arr=()=>{
+  const heading=document.addEventListener("click",()=>{console.log("dharsh");
+})
+   const container=document.getElementById("container");
+   container.appendChild(heading);
+ }
+               
+document.addEventListener("click",()=>{
+console.log("dharsh");
+})
+
+const col=function(){
+const div=document.getElementById("cont")
+div.style.backgroundColor="black"
+}
